824f471024fda9805495ad1742517cfbe5c170d2
npax1 <prakh55555@gmail.com>
2022-09-27 20:27:03 +0300

    Update: Add code "cool cifri"

--- ex_1.py
+++ ex_1.py
@@ -2,6 +2,11 @@ num_1=int(input("ціну приставку"))
 num_2=int(input("кількість приставок"))
 num_3=int(input("скидка в %"))
 num_4=int(input("яку операцію \n1 сума всіх приставок\n2 одна приставка з скидкою "))
-if num_4=1:
+if num_4==1:
     a=num_1*num_2
     print(a)
+elif num_4 == 2:
+    s=num_3/num_1/100
+    print(s)
+else:
+    print("erroooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooore")
