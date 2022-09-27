a3dd1f79251110bdb237c8467c9d1a50b1d8bdb5
npax1 <prakh55555@gmail.com>
2022-09-27 20:07:35 +0300

    Update: Add code "colo"

--- ex_1.py
+++ ex_1.py
@@ -1,1 +1,8 @@
-print ("world")
+num_1=int(input("веди діаметр"))
+x=3.14
+s=int(input("яка операція \n 1периметр \n 2площа"))
+if s==1:
+    p=(x*num_1*num_1)
+if s ==2:
+    p=(x*num_1*2)
+print(f'p={p}')
