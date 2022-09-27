2b35a5ed8b523604e75a7ae8fb7127935a77b593
npax1 <prakh55555@gmail.com>
2022-09-27 19:53:32 +0300

    Update: Add code"coll code"

--- ex_1.py
+++ ex_1.py
@@ -1,15 +1,1 @@
-a=int(input("ведіть час в секунди"))
-q=int(input("ведіть час в 1 2 xb 3"))
-if q==1:
-    s=a-86400
-    print(s)
-elif q==2:
-    d=a/60
-    f=d-1440
-    print(f)
-elif q==3:
-    g=a/3600
-    h=g-24
-    print(h)
-else:
-    print("введи 1 2 чи 3")
+print ("world")
